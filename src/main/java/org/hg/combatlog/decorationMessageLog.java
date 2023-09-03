dc73a5543f1f14ec5eb2537dd44bf0fc3fad5bf5
iron-blooded <ayss2@mail.ru>
2023-09-03 23:47:07 +1200

    сделал при наведении на страницу превью команды

--- src/main/java/org/hg/combatlog/decorationMessageLog.java
+++ src/main/java/org/hg/combatlog/decorationMessageLog.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.util.List;
 
 import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
 
@@ -21,7 +22,7 @@ public class decorationMessageLog {
         if (page <= 4){
             for (int i = 1; i <= 7 && i <= max_page; i++){
                 TextComponent hover_page = new TextComponent(ChatColor.RESET+"");
-                hover_page.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/combat_page "+i));
+                hoverCombatPageSet(hover_page, "/combat_page "+i);
                 if (page == i){
                     hover_page.addExtra(new TextComponent(ChatColor.RESET+"|"+ChatColor.UNDERLINE+page+ChatColor.RESET+"| "));
                     hover_page.setColor(net.md_5.bungee.api.ChatColor.GOLD);
@@ -33,18 +34,18 @@ public class decorationMessageLog {
             }
             if (max_page > 7) {
                 TextComponent hover_page = new TextComponent(ChatColor.RESET+""+max_page);
-                hover_page.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/combat_page "+max_page));
+                hoverCombatPageSet(hover_page, "/combat_page "+max_page);
                 str.addExtra(new TextComponent(ChatColor.RESET + "... "));
                 str.addExtra(hover_page);
             }
         } else if (page+2>=max_page) {
             TextComponent hover_page = new TextComponent(ChatColor.RESET+"1");
-            hover_page.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/combat_page 1"));
+            hoverCombatPageSet(hover_page, "/combat_page 1");
             str.addExtra(hover_page);
             str.addExtra(new TextComponent(ChatColor.RESET + " ... "));
             for (int i = max_page-4; i <= max_page; i++){
                 hover_page = new TextComponent(ChatColor.RESET+"");
-                hover_page.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/combat_page "+i));
+                hoverCombatPageSet(hover_page, "/combat_page "+i);
                 if (page == i){
                     hover_page.addExtra(new TextComponent(ChatColor.RESET+"|"+ChatColor.UNDERLINE+page+ChatColor.RESET+"| "));
                     hover_page.setColor(net.md_5.bungee.api.ChatColor.GOLD);
@@ -57,13 +58,13 @@ public class decorationMessageLog {
         }
         else {
             TextComponent hover_page = new TextComponent(ChatColor.RESET+"1");
-            hover_page.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/combat_page 1"));
+            hoverCombatPageSet(hover_page, "/combat_page 1");
             str.addExtra(hover_page);
             str.addExtra(new TextComponent(ChatColor.RESET + " ... "));
             /// ...
             for (int i = page-2; i <= page + 2; i++){
                 hover_page = new TextComponent(ChatColor.RESET+"");
-                hover_page.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/combat_page "+i));
+                hoverCombatPageSet(hover_page, "/combat_page "+i);
                 if (page == i){
                     hover_page.addExtra(new TextComponent(ChatColor.RESET+"|"+ChatColor.UNDERLINE+page+ChatColor.RESET+"| "));
                     hover_page.setColor(net.md_5.bungee.api.ChatColor.GOLD);
@@ -75,7 +76,7 @@ public class decorationMessageLog {
             }
             /// ...
             hover_page = new TextComponent(ChatColor.RESET+""+max_page);
-            hover_page.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/combat_page "+max_page));
+            hoverCombatPageSet(hover_page, "/combat_page "+max_page);
             str.addExtra(new TextComponent(ChatColor.RESET + "... "));
             str.addExtra(hover_page);
         }
@@ -83,5 +84,9 @@ public class decorationMessageLog {
         str.addExtra(new TextComponent(ChatColor.AQUA+"】╝"));
         messages.add(str);
         return;
+    }
+    private void hoverCombatPageSet(TextComponent hover_page, String name){
+        hover_page.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND,name));
+        hover_page.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(name)));
     }
 }
